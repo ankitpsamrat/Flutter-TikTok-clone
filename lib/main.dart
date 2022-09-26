3e3a2418d0d38ec776b98d2de21f93888cf847b8
ankitpsamrat <ankit.p.samrat@gmail.com>
2022-09-26 09:12:41 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/controller/auth_controller.dart';
+import 'package:tiktok_clone/firebase_options.dart';
 import 'package:tiktok_clone/view/screens/auth/signup_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((value) => Get.put(AuthController()));
   runApp(const MyApp());
 }
 
@@ -11,7 +19,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Tik Tok',
       theme: ThemeData.dark().copyWith(
---
+++ lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class User {
+   String name;
+  String profilePhoto;
+  String email;
+  String uid;
+
+  User({
+    required this.name,
+    required this.profilePhoto,
+    required this.email,
+    required this.uid
+  })
+}
